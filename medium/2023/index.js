7af2c67acdce8c50d7aceca82c48f8da183499a2
shikharka <shikhar.k.a@gmail.com>
2022-10-17 09:31:12 +0530

    Day 9 optimized

--- medium/2023/index.js
+++ medium/2023/index.js
@@ -6,28 +6,29 @@
 
 var numOfPairs = function (nums, target) {
   return nums.reduce((counter, num, index) => {
-    let otherPart = "";
     if (target.startsWith(num)) {
-      otherPart = target.slice(num.length);
-    }
-    let numsCopy = new Array(...nums);
-    numsCopy.splice(index, 1);
+      const otherPart = target.slice(num.length);
 
-    const otherPartCount = numsCopy.reduce(
-      (count, n) => (otherPart === n ? ++count : count),
-      0
-    );
-    return counter + otherPartCount;
+      let numsCopy = new Array(...nums);
+      numsCopy.splice(index, 1);
+
+      const otherPartCount = numsCopy.reduce(
+        (count, n) => (otherPart === n ? ++count : count),
+        0
+      );
+      return counter + otherPartCount;
+    }
+    return counter;
   }, 0);
 };
 
-const nums = ["777", "7", "77", "77"],
-  target = "7777";
+// const nums = ["777", "7", "77", "77"],
+//   target = "7777";
 
 // const nums = ["123", "4", "12", "34"],
 //   target = "1234";
 
-// const nums = ["1", "1", "1"],
-//   target = "11";
+const nums = ["1", "1", "1"],
+  target = "11";
 
 console.log(numOfPairs(nums, target));
