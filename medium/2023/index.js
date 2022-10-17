cc250845bee1413ad659768be2a6252daee3e19e
shikharka <shikhar.k.a@gmail.com>
2022-10-17 09:38:00 +0530

    Day 9 Comments added

--- medium/2023/index.js
+++ medium/2023/index.js
@@ -6,16 +6,22 @@
 
 var numOfPairs = function (nums, target) {
   return nums.reduce((counter, num, index) => {
+    // check if target starts with num, if not skip
     if (target.startsWith(num)) {
+      // cut out remaining from target
       const otherPart = target.slice(num.length);
 
+      // nums copy created and current num omitted
       let numsCopy = new Array(...nums);
       numsCopy.splice(index, 1);
 
+      //  count otherPart in numsCopy
       const otherPartCount = numsCopy.reduce(
         (count, n) => (otherPart === n ? ++count : count),
         0
       );
+
+      // add otherPartCount to final counter
       return counter + otherPartCount;
     }
     return counter;
